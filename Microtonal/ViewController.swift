1423987e296977598c36d738eac84655b5e87dad
Yaroslav Erohin <ysoftware@users.noreply.github.com>
2017-09-10 19:01:18 +0300

    Initial Commit

---
+++ Microtonal/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Microtonal
+//
+//  Created by ysoftware on 10.09.17.
+//  Copyright © 2017 ysoftware. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
